f14f0a376c1d9c21315d8eaa7b0d61faf119a9e9
大谨 <mccoach@126.com>
2025-07-04 22:26:26 +0800

    fix: add path resolve import in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'   // ← ← ← ← 这行是关键！
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/visualtime/', // 替换为你的仓库名，这里只需要仓库名本身，不需要完整路径，前后都要有斜杠
+  base: '/visualtime/',
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
